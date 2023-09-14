ddfb3cfe0d8e5d0919ff545391b0837eea286024
HKplus <1656800105@qq.com>
2023-09-14 16:26:05 +0800

    reset main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-const greeting = "合并测试 1 develop 再 greeting 后进行了修改";
-=======
-const greeting = "分支合并测试 greeting 的修改结果";
->>>>>>> greeting
-console.log(greeting);
+const message = "reset main.js";
+console.log(message)
