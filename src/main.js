acdbc66b433416bbc0e8a57e519c292a9be8c1e4
HKplus <1656800105@qq.com>
2023-09-15 10:39:03 +0800

    请求与相应

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 const http = require("http");
 
-const server = http.createServer((request , response) => {
-	response.write("你好");
+const server = http.createServer((request, response) => {
+
+	// 需要通过响应头告诉计算机相应的 html , 浏览器才能正确的处理
+	response.writeHead(200, {
+		'Content-Type': 'text/html',
+	})
+	// 响应一个 html 代码
+	response.write('<input type="button" value="点击">');
 	response.end();
 });
 
-server.listen(3000 , ()=>{
+server.listen(3000, () => {
 	console.log("^_^ 服务已开启");
 })
