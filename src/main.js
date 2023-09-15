110cf272e8e0c1b4ff776685456cfc360b6c7166
HKplus <1656800105@qq.com>
2023-09-15 09:55:25 +0800

    creat  web-server

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
-const message = "reset main.js";
-console.log(message)
+const http = require("http");
+
+const server = http.createServer((request , response) => {
+	response.write("你好");
+	response.end();
+});
+
+server.listen(3000 , ()=>{
+	console.log("^_^ 服务已开启");
+})
