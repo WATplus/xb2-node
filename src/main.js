096cbd2beac2e777020e356f3ebd0006d1333a08
HKplus <1656800105@qq.com>
2023-09-16 22:48:24 +0800

    创建简单的 express 服务

--- src/main.js
+++ src/main.js
@@ -1,16 +1,46 @@
-const http = require("http");
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((request, response) => {
+app.listen(port, () => {
+	console.log("^_^ 服务已启动");
+})
 
-	// 需要通过响应头告诉计算机相应的 html , 浏览器才能正确的处理
-	response.writeHead(200, {
-		'Content-Type': 'text/html',
-	})
-	// 响应一个 html 代码
-	response.write('<input type="button" value="点击">');
-	response.end();
-});
+// 定义一个通过 HTTP 的 get方式访问时的接口
+app.get("/", (request, response) => {
+	response.send("你好");
+})
 
-server.listen(3000, () => {
-	console.log("^_^ 服务已开启");
+const data = [
+	{
+		id: 1,
+		title: "观月山",
+		content: '云月出天山,苍茫云海间',
+	},
+	{
+		id: 2,
+		title: "望岳",
+		content: '会当凌绝顶,一览众山小',
+	},
+	{
+		id: 2,
+		title: "忆江南",
+		content: '日出江花红胜火,春来江水绿如蓝',
+	},
+]
+
+app.get("/posts", (request, response) => {
+	response.send(data)
+})
+
+// 带参数的接口
+app.get("/posts/:postsId", (request, response) => {
+	// 获取参数
+	const { postsId } = request.params;
+
+	// 过滤出 id 对应的项
+	const posts = data.filter(item => item.id == postsId);
+
+	// 响应
+	response.send(posts);
 })
