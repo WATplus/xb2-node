3ccc488e494ffc0883916b4e4c466c7fa0dbe70b
HKplus <1656800105@qq.com>
2023-09-22 14:20:51 +0800

    数据库链接

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,3 +6,15 @@ dotenv.config(); // 默认读取主目录下的.env
  * 应用配置
  */
 export const {APP_PORT} = process.env;
+
+/**
+ * 数据库相关配置
+*/
+
+export const {
+    MYSQL_HOST,
+    MYSQL_PORT,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    MYSQL_DATABASE,
+} = process.env;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import app from "./app" // 这里不用/index.ts node 会自动指向 index.ts
 import { APP_PORT } from "./app/app.config";
+import { connection } from "./app/database/mysql";
 
 /**
  * 开启服务
@@ -7,4 +8,15 @@ import { APP_PORT } from "./app/app.config";
 app.listen(APP_PORT , ()=>{
     console.log(`^_^ 服务已开启 , 端口: ${APP_PORT}`);
     
+})
+
+/** 
+ * 测试数据仓库的连接
+ */
+connection.connect(error => {
+    if (error) {
+        console.log("T_T 数据库服务连接失败" , error.message);
+        return;
+    }
+    console.log("@^_^@ 数据库服务链接成功");
 })
