ffbd1f91f1c38bb6dd93e2f400ff809948fb6276
HKplus <1656800105@qq.com>
2023-09-27 16:22:06 +0800

    模块准备

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import postRouter from "../post/post.router";
 import userRouter from '../user/user.router'
 import { defaultErrorHendler } from "./app.middleware";
+import authRouter from '../auth/auth.router'
 
 /**
  * 创建应用
@@ -16,7 +17,7 @@ app.use(express.json())
 /**
  * 接口
 */
-app.use(postRouter,userRouter)// 注意是 use , 因为接口被创建的时候就已经定义好类型了
+app.use(postRouter,userRouter,authRouter)// 注意是 use , 因为接口被创建的时候就已经定义好类型了
 /**
  * 错误处理器
 */
---
+++ src/auth/auth.contorller.ts
@@ -0,0 +1,3 @@
+import {Request , Response , NextFunction} from 'express'
+
+// import * as authService from "./auth.service"
